b03834bf745edf8328829f3d473d70c0c9366f32
Rosa Ariza <rosaarizag@gmail.com>
2024-05-27 10:36:58 -0500

    proyecto final

---
+++ lib/screens/screens.dart
@@ -0,0 +1,12 @@
+
+export 'package:green_project/screens/Info/components/tema_screen.dart';
+export 'package:green_project/screens/Info/info_screen.dart';
+export 'package:green_project/screens/Ubication/ubication_screen.dart';
+export 'package:green_project/screens/home/home_screen.dart';
+export 'package:green_project/screens/login/login_screen.dart';
+export 'package:green_project/screens/welcome/welcome_screen.dart';
+export 'package:green_project/screens/Detection/detection_screen.dart';
+export 'package:green_project/screens/Account/account_screen.dart';
+export 'package:green_project/screens/Detection/components/detalle_screen.dart';
+
+
